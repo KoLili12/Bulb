c41888bc48713e2edbb9d49c23ce892ea9b44318
NickZhirnoff <nickzhirnoff@gmail.com>
2025-03-25 14:38:29 +0300

    Добавил TabBarViewController

---
+++ Bulb/TabBarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarViewController.swift
+//  Bulb
+//
+//  Created by Николай Жирнов on 25.03.2025.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
