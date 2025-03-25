a31466424de6fc91214ea56f996d50e1d247b366
NickZhirnoff <nickzhirnoff@gmail.com>
2025-03-25 15:39:45 +0300

    добавил другие viewControllers

--- Bulb/TabBarViewController.swift
+++ Bulb/TabBarViewController.swift
@@ -10,5 +10,27 @@ import UIKit
 class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let homeViewController = HomeViewController()
+        let profileViewController = ProfileViewController()
+        
+        
+        homeViewController.tabBarItem = UITabBarItem(
+            title: "Лента",
+            image: UIImage(named: "HomeIcon"),
+            selectedImage: nil
+        )
+        
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "Профиль",
+            image: UIImage(named: "ProfileIcon"),
+            selectedImage: nil
+        )
+        
+        homeViewController.tabBarItem.imageInsets =  UIEdgeInsets(top: 5, left: 15, bottom: 15, right: 15)
+        profileViewController.tabBarItem.imageInsets =  UIEdgeInsets(top: 1, left: 10, bottom: 15, right: 10)
+        
+        UITabBar.appearance().tintColor = UIColor(named: "violetBulb")
+        self.viewControllers = [homeViewController, profileViewController]
     }
 }
