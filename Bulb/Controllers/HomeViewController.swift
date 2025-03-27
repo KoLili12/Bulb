46bd1fcb96b51798d5dfe746ad2d714fe45156f9
NickZhirnoff <nickzhirnoff@gmail.com>
2025-03-27 15:57:23 +0300

    Добавил два новых контроллера в TabBarViewController

---
+++ Bulb/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Bulb
+//
+//  Created by Николай Жирнов on 25.03.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+}
---
+++ Bulb/Controllers/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Bulb
+//
+//  Created by Николай Жирнов on 25.03.2025.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+}
